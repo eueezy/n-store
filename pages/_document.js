c33b7a7175f1a98cb3634ded264a07bdeaf8638c
eueezy <46291404+eueezy@users.noreply.github.com>
2019-01-05 18:20:46 +0900

    4.2 Auditing with Light House part One

--- pages/_document.js
+++ pages/_document.js
@@ -7,14 +7,17 @@ export default class MyDocument extends Document {
   }
   render() {
     return (
-      <html>
+      <html lang="en">
         <Head>
           <meta name="author" content={"Euijin"} />
           <link
-            href="//cdnjs.cloudflare.com/ajax/libs/antd/3.11.6/antd.min.css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.11.6/antd.min.css"
             rel="stylesheet"
           />
           <style>{`body { background-color: #EFF2F5!important}`}</style>
+          <meta name="theme-color" content="black" />
+          <meta name="viewport" content="width=device-width, initail-scale=1" />
+          <meta name="description" content="N-Store, buy all your nomad swag" />
         </Head>
         <body>
           <Main />
@@ -24,3 +27,12 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+// document 수정하면 서버 재시작 필요
+
+/* 
+meta name viewport -> 웹사이트가 어디서 보여지고 있는지 의미
+device-width : width가 디바이스의 width와 같음
+
+
+*/
