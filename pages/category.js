7057b960ca2e7fb18c14884daaf018ba04cc9910
eueezy <46291404+eueezy@users.noreply.github.com>
2019-01-05 17:41:11 +0900

    정리

--- pages/category.js
+++
@@ -1,1 +0,0 @@
-export default () => <h1>category</h1>;
--- resolvers.js
+++ resolvers.js
@@ -8,12 +8,14 @@ export const defaults = {
 export const resolvers = {
   Mutation: {
     toggleProduct: (_, variables, { cache, getCacheKey }) => {
+      // product 가져오기
       const id = getCacheKey({ __typename: "Product", id: variables.id });
       const fragment = gql`
         ${PRODUCT_FRAGMENT}
       `;
       const product = cache.readFragment({ fragment, id });
 
+      // cart 가져오기
       const cartQuery = gql`
         {
           cart @client {
@@ -22,23 +24,31 @@ export const resolvers = {
         }
       `;
       const { cart } = cache.readQuery({ query: cartQuery });
+
+      // cart 확인
       let newCart;
       let onCart;
       const foundProduct = cart.find(aProduct => aProduct.id === product.id);
+      // find -> cart[] 에 있는 모든 item을 받음 -> 현재 아이템을 argument로 받음 -> aProduct
       if (foundProduct) {
         const cleanCart = cart.filter(aProduct => aProduct.id !== product.id);
-        newCart = cleanCart;
+        // filter -> cart[] 에 있는 모든 item을 받음 -> 조건에 맞는 새로운 array를 return
+        newCart = cleanCart; // 해당 id의 product만 삭제
         onCart = false;
       } else {
         newCart = [...cart, product];
         onCart = true;
       }
+      // find -> 조건에 만족하는 item을 return / filter -> 조건에 만족하는 item이 담긴 array를 return
 
+      // product -> cart []
       cache.writeData({
         data: {
-          cart: newCart
+          cart: newCart // [...cart, product]
         }
       });
+
+      // fragment 수정 (onCart)
       cache.writeFragment({
         id: `Product:${product.id}`,
         fragment: PRODUCT_FRAGMENT,
@@ -51,6 +61,7 @@ export const resolvers = {
       return null;
     }
   },
+  // API state 수정 작업 -> productQueries에 onCart 추가
   Product: {
     onCart: () => false
   }
